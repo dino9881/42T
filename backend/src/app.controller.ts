1e6b1690a9d54fb9b6481fe8ed08161f50085dc2
lamPolar <lovast7@gmail.com>
2023-05-13 15:13:16 +0900

    [BE][Feat] : prisma schema 작성완료

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,10 @@
 import { Controller, Get, Post, Body, Query, NotFoundException } from '@nestjs/common';
 import { AppService } from './app.service';
-import { UserService } from './user.service';
-import {User as UserModel} from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly userService: UserService,
     ) {}
 
   @Get()
@@ -16,20 +13,4 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post('user')
-  async signupUser(
-    @Body() userData: { name?: string; email: string},
-  ): Promise<UserModel> {
-    return this.userService.createUser(userData);
-  }
-
-  @Get('name')
-  async getName(@Query('id') id: string): Promise<string> {
-    const user = await this.userService.user({ id: Number(id) });
-    if (user) {
-      return user.name;
-    } else {
-      throw new NotFoundException(`User with ID ${id} not found`);
-    }
-  }
 }
