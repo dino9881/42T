c9ca1a0e86e852de6a8c13a7bfa1ab06d3e7b363
lamPolar <lovast7@gmail.com>
2023-05-13 13:29:34 +0900

    [BE][Test] : swagger도입완료

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const logger = new Logger('App');
 async function bootstrap() {
@@ -12,6 +13,15 @@ async function bootstrap() {
         credentials:true,
       },
     });
+    const config = new DocumentBuilder()
+    .setTitle('42T')
+    .setDescription('The 42seoul Transcendence API description')
+    .setVersion('1.0')
+    .addTag('Member')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(5001);
 
     const prismaService = app.get(PrismaService);
