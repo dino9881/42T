5f143ee2e331148dcc709d312dfc876c43d949d5
lamPolar <lovast7@gmail.com>
2023-05-13 12:54:41 +0900

    [BE][TEST] : prisma 도커 테스트2

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,11 @@
 
 # wait for the postgres container to be ready
 # wait-for-it postgres:5432 -t 60 -- echo "postgres is up"
+curl -o wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
+chmod +x wait-for-it.sh
+./wait-for-it.sh postgres:5432 -- echo "PostgreSQL is up and running!"
+
+export PRISMA_MIGRATE_INTERACTIVE=true
 
 # generate the Prisma Client
 npx prisma migrate dev --name init && \
