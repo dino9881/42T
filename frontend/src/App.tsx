2eceff38be13d671dd5ca79eb06e7e53a7be13e6
lamPolar <lovast7@gmail.com>
2023-05-20 15:24:51 +0900

    [BE][Merge] : login-oauth브랜치 front-end 파일병합

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,24 @@
-import React, { useState } from 'react';
-import './App.css';
-import axios, {AxiosResponse} from 'axios';
+import "./App.css";
+import Layout from "./Layout";
+import Login from "./Login";
+import Main from "./main";
+import Chat from "./Chat";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import OAuth from "./OAuth";
 
 function App() {
-
-  const [name, setName] = useState("");
-  const [getName, setGetName] = useState("");
-  
-  function onClick() : void {
-    const res = axios.post('http://localhost:5001/user', {"name": name, "email": 'l@Gmail.com'});
-    console.log(res);
-  }
-
-  function inputText(e : React.ChangeEvent<HTMLInputElement>) {
-    setName(e.target.value);
-    console.log(name);
-  }
-  
-  function handleGetClick() : void{
-    axios.get('http://localhost:5001/name', {params: {id : 1}})
-    .then((res : AxiosResponse<string>)=>{
-      setGetName(res.data);
-      console.log(res);
-    })
-  }
-
-  return (
-    <div className="App">
-     <input  onChange={inputText} value={name}></input>
-     <button onClick={onClick} >click</button>
-     <h1>text : {name}</h1>
-     <>
-     <button onClick={handleGetClick} >get id = 1</button>
-     <h1>get id == 1 name: {getName}</h1>
-     </>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />}></Route>
+                    <Route path="/login" element={<OAuth />}></Route>
+                    <Route path="/main" element={<Main />}></Route>
+                    <Route path="/chat" element={<Chat />}></Route>
+                </Routes>
+            </BrowserRouter>
+            <Layout />
+        </div>
+    );
 }
-
 export default App;
