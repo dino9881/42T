946cf1eadd8c89fd1d96685b6c4a8fbe00f99a62
lamPolar <lovast7@gmail.com>
2023-05-20 15:24:51 +0900

    [CM][Feat] intra login 구현 (access token 받아오기 완료)

--- backend/src/member/member.module.ts
+++ backend/src/member/member.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { MemberController } from './member.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   imports: [],
   controllers: [MemberController],
-  providers: [MemberService],
+  providers: [MemberService, PrismaService],
 })
 export class MemberModule {}
