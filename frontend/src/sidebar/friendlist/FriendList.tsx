b4c096049d1e810ad0beba923751cbfccaa976d8
YH-Yoo <yehuen981@naver.com>
2023-05-20 13:43:45 +0900

    [FE][Feat] : 개인프로필작성

--- frontend/src/sidebar/friendlist/FriendList.tsx
+++ frontend/src/sidebar/friendlist/FriendList.tsx
@@ -2,12 +2,24 @@ import React from "react";
 import './FriendList.css';
 import FriendInfo from "./FriendInfo";
 
-const FriendList = () => {
+interface Friend {
+	name: string;
+	age: number;
+	avatarUrl: string;
+}
+  
+interface FriendListProps {
+	friends: Friend[];
+}
+
+const FriendList: React.FC<FriendListProps> = ({ friends }) => {
+  
 	return (
-		<div>
-			<FriendInfo />
-			<FriendInfo />
-		</div>
+	  <div className="list-scroll">
+		{friends.map((friend, index) => (
+        <FriendInfo key={index} name={friend.name} age={friend.age} avatarUrl={friend.avatarUrl}/>
+     	 ))}
+	  </div>
 	);
 };
 
--- frontend/src/sidebar/myinfo/MyInfo.tsx
+++ frontend/src/sidebar/myinfo/MyInfo.tsx
@@ -1,23 +1,33 @@
 import React from "react";
 import './MyInfo.css';
 
-const MyInfo = () => {
+interface Myinfo {
+	name: string;
+	age: number;
+	avatarUrl: string;
+}
+  
+interface MyInfoProps {
+	myinfo: Myinfo;
+}
+
+const MyInfo: React.FC<MyInfoProps> = ({ myinfo }) => {
+	console.log(myinfo.avatarUrl);
 	return (
 		<div className='my-info'>
 			<div className='my-info-line'>
 				<div className='state-circle'></div>
-				<div className='my-info-avatar'></div>
+				<div className='my-info-avatar'
+					style={{ backgroundImage: `url(${myinfo.avatarUrl})` }}
+				></div>
 				<div className='my-info-info'>
 					<div className='my-info-text'>
-						<div className='small-square'>nick</div>
-						<div className='small-square'>LANK</div>
+						<div className='small-square'>{myinfo.name}</div>
+						<div className='small-square'>{myinfo.age}</div>
 					</div>
 					<div className='my-info-button'>
 						<button className='small-square'>수정</button>
-						<div className='with-triangle'>
-							<button className='small-square'>전적</button>
-							<button className='inverted-triangle'>삼</button>
-						</div>
+						<button className='small-square'>전적</button>
 					</div>
 				</div>
 			</div>
