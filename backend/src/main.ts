a04fd62cfaceae8f013d1edfda6886fffcd59683
lamPolar <lovast7@gmail.com>
2023-05-26 18:48:06 +0900

    [BE][Design] : swagger document description추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -17,10 +17,16 @@ async function bootstrap() {
       .setTitle('42T')
       .setDescription('The 42seoul Transcendence API description')
       .setVersion('1.0')
-      .addTag('Member')
+      .addTag('app', '')
+      .setDescription(
+        'The 42seoul Transcendence API description<br/><br/><a href="https://insomnia.rest/run/?label=My%20API&uri=http%3A%2F%2Flocalhost%3A5001%2Fapi-json" target="_blank"><img src="https://insomnia.rest/images/run.svg" alt="Run in Insomnia"></a>',
+      )
+      .setExternalDoc('Postman Collection', '/api-json')
       .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: { defaultModelsExpandDepth: -1 },
+    });
 
     await app.listen(5001);
 
