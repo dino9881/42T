eb99df0f7cff37811f851e28a61140fab175f3ff
lamPolar <lovast7@gmail.com>
2023-05-27 14:56:54 +0900

    [BE][Feat] : jwt module 생성

--- backend/src/member/member.module.ts
+++ backend/src/member/member.module.ts
@@ -7,5 +7,6 @@ import { PrismaService } from 'src/prisma.service';
   imports: [],
   controllers: [MemberController],
   providers: [MemberService, PrismaService],
+  exports: [MemberService],
 })
 export class MemberModule {}
