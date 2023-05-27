b43088d92c1f1e7b6ae7430a9a21438e0876e09f
lamPolar <lovast7@gmail.com>
2023-05-27 12:01:13 +0900

    [BE][Merge] : channel-module & member-module 병합

--- backend/src/channel/channel.controller.ts
+++ backend/src/channel/channel.controller.ts
@@ -1,52 +1,90 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { CreateChannelDto } from './dto/create-channel.dto';
 import { UpdateChannelDto } from './dto/update-channel.dto';
+import { ApiTags, ApiOperation, ApiCreatedResponse, ApiResponse} from '@nestjs/swagger';
 
 @Controller('channel')
+@ApiTags('Channel API')
 export class ChannelController {
   constructor(private readonly channelService: ChannelService) {}
 
-  @Post('create')
+  @Post('/create')
+  @ApiOperation({ summary: '채널 생성', description: 'Create Channel API' })
+  @ApiResponse({status: 200, description: "성공"})
+  @ApiResponse({status: 409, description: "중복 이름"})
+  @ApiResponse({status: 500, description: "서버 에러"})
+  @ApiCreatedResponse({ type: CreateChannelDto })
   create(@Body() createChannelDto: CreateChannelDto) {
+    console.log(createChannelDto);
     return this.channelService.create(createChannelDto);
   }
 
-  @Get('all')
-  findAll() {
-    return this.channelService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.channelService.findOne(+id);
-  }
-
   @Patch(':id')
+  @ApiOperation({ summary: '채널 수정', description: 'Update Channel API' })
+  @ApiResponse({status: 200, description: "성공"})
+  @ApiResponse({status: 404, description: "없는 채널 번호"})
+  @ApiResponse({status: 409, description: "중복 이름"})
+  @ApiResponse({status: 500, description: "서버 에러"})
   update(@Param('id') id: string, @Body() updateChannelDto: UpdateChannelDto) {
     return this.channelService.update(+id, updateChannelDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.channelService.remove(+id);
+  @ApiResponse({status: 200, description: "성공"})
+  @ApiResponse({status: 404, description: "없는 채널 번호"})
+  @ApiResponse({status: 500, description: "서버 에러"})
+  @ApiOperation({ summary: '채널 삭제', description: 'Delete Channel API' })
+  delete(@Param('id') id: string) {
+    return this.channelService.delete(+id);
   }
 
-  @Post('enter:id')
-  enterChannel(@Body() intraId: string, @Param('id') id: string) {
-    // return this.channelService.enter(id, intraId);
+  @Get('/all')
+  @ApiResponse({status: 200, description: "성공"})
+  @ApiResponse({status: 404, description: "없는 채널 번호"})
+  @ApiResponse({status: 500, description: "서버 에러"})
+  @ApiOperation({ summary: '모든 채널 조회', description: 'Find All Channel API' })
+  findAll() {
+    return this.channelService.findAll();
   }
 
-  @Post('leave:id')
-  leaveChannel(@Body() intraId: string, @Param('id') id: string) {
-    //return this.channelService.leave(id, intraId);
+  @Get(':id')
+  @ApiResponse({status: 200, description: "성공"})
+  @ApiResponse({status: 404, description: "없는 채널 번호"})
+  @ApiResponse({status: 500, description: "서버 에러"})
+  @ApiOperation({ summary: 'idx 로 한 채널 가져오기', description: 'Find channel By Idx API' })
+  findOneById(@Param('id') id: string) {
+    return this.channelService.findOneById(+id);
   }
+
+  @Get('/name/:id')
+  @ApiResponse({status: 200, description: "성공"})
+  @ApiResponse({status: 404, description: "없는 채널 번호"})
+  @ApiResponse({status: 500, description: "서버 에러"})
+  @ApiOperation({ summary: 'idx 로 채널 이름 가져오기', description: 'Get channel name By Idx API' })
+  getChannelName(@Param('id') id: string) {
+    return this.channelService.getChannelName(+id);
+  }
+
+  @Get('/user_cnt/:id')
+  @ApiResponse({status: 200, description: "성공"})
+  @ApiResponse({status: 404, description: "없는 채널 번호"})
+  @ApiResponse({status: 500, description: "서버 에러"})
+  @ApiOperation({ summary: 'idx 로 채널 인원 가져오기', description: 'Get channel user count By Idx API' })
+  getChannelUserCnt(@Param('id') id: string) {
+    return this.channelService.getChannelUserCnt(+id);
+  }
+
+  
+  // @Post('/enter/:id')
+  // @ApiOperation({ summary: 'idx 채널에 들어가기', description: 'Enter channel By Idx API' })
+  // enterChannel(@Body() intraId: string, @Param('id') id: string) {
+  //   this.channelService.enter(+id, intraId);
+  // }
+
+  // @Post('leave:id')
+  // @ApiOperation({ summary: 'idx 채널에서 나오기', description: 'Leave channel By Idx API' })
+  // leaveChannel(@Body() intraId: string, @Param('id') id: string) {
+  //   this.channelService.leave(+id, intraId);
+  // }
 }
--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -1,26 +1,130 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateChannelDto } from './dto/create-channel.dto';
 import { UpdateChannelDto } from './dto/update-channel.dto';
+import { PrismaService } from 'src/prisma.service';
+import { Channel, Prisma } from '@prisma/client';
+
 
 @Injectable()
 export class ChannelService {
-  create(createChannelDto: CreateChannelDto) {
-    return 'This action adds a new channel';
+  constructor(private prisma: PrismaService) {}
+  
+  async create(createChannelDto: CreateChannelDto) {
+    var createData;
+    try {
+      const {chName, intraId, chPwd} = createChannelDto;
+      createData = await this.prisma.channel.create({
+        data: { chName, chUserCnt: 0, intraId, chPwd },
+      });
+    } catch (error) {
+      if (error.code === 'P2002' && error.meta?.target?.includes('chName')) {
+        throw new ConflictException('Duplicate chName');
+      }
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    return createData;
   }
 
-  findAll() {
-    return `This action returns all channel`;
+  async update(id: number, updateChannelDto: UpdateChannelDto) {
+    var updateData;
+    try {
+      const {chName, intraId, chPwd} = updateChannelDto;
+      updateData = await this.prisma.channel.update({ 
+        where: { chIdx: id }, 
+        data: {chName, intraId, chPwd} 
+      });
+    } catch (error) {
+      if (error.code === 'P2002' && error.meta?.target?.includes('chName')) {
+        throw new ConflictException('Duplicate chName');
+      }
+      if (error.code === 'P2016' || error.code === 'P2025') {
+        throw new NotFoundException('channel not found');
+      }
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    return updateData;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} channel`;
+  async delete(id: number) {
+    var deleteData;
+    try {
+      deleteData = await this.prisma.channel.delete({ where: { chIdx: id} });
+    } catch (error) {
+      if (error.code === 'P2016' || error.code === 'P2025') {
+        throw new NotFoundException('channel not found');
+      }
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    return deleteData;
   }
 
-  update(id: number, updateChannelDto: UpdateChannelDto) {
-    return `This action updates a #${id} channel`;
+  async findAll() {
+    return this.prisma.channel.findMany();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} channel`;
+  async findOneById(id: number) {
+    var findData;
+    try {
+      findData = await this.prisma.channel.findUnique({ 
+        where: { chIdx: id } });
+    } catch (error) {
+      if (error.code === 'P2016' || error.code === 'P2025') {
+        throw new NotFoundException('channel not found');
+      }
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    if (findData == null)
+        throw new NotFoundException('channel not found');
+    return findData;
   }
+
+  async getChannelName(id: number) {
+    var findData;
+    try {
+      findData = await this.prisma.channel.findUnique({ 
+        where: { chIdx: id },
+        select: { chName: true }
+      });
+    } catch (error) {
+      if (error.code === 'P2016' || error.code === 'P2025') {
+        throw new NotFoundException('channel not found');
+      }
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    if (findData == null)
+        throw new NotFoundException('channel not found');
+    return findData;
+  }
+
+  async getChannelUserCnt(id: number) {
+    var findData;
+    try {
+      findData = await this.prisma.channel.findUnique({ 
+        where: { chIdx: id },
+        select: { chUserCnt: true }
+      });
+    } catch (error) {
+      if (error.code === 'P2016' || error.code === 'P2025') {
+        throw new NotFoundException('channel not found');
+      }
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    if (findData == null)
+        throw new NotFoundException('channel not found');
+    return findData;
+  }
+
+  async checkPwd(pwd: string) {
+
+  }
+
+  async enter(id: number, intraId: string) {
+    // pwd 검사
+    return this.prisma.channel.update({ where: { chIdx: id }, data: { intraId: intraId }});
+  }
+
+  // async leave(id: number, intraId: string) {
+  //   return this.prisma.channel.delete({ where: { chIdx: id }, data: { intraId: null }});
+  // }
+  
 }
