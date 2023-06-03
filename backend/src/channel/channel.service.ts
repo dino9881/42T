24c3141d4d99338271f3b7c787bbfd6edf3c1f3d
Haeun Jeong <gkdms4248@gmail.com>
2023-06-03 18:13:14 +0900

    [BE][Test] make userlist array in channel

--- backend/src/channel/channel.service.ts
+++ backend/src/channel/channel.service.ts
@@ -1,20 +1,31 @@
-import { Injectable, ConflictException, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateChannelDto } from './dto/create-channel.dto';
 import { UpdateChannelDto } from './dto/update-channel.dto';
 import { PrismaService } from 'src/prisma.service';
 import { Channel, Prisma } from '@prisma/client';
-
+import { MemberIdDto } from './dto/member-id.dto';
+import { memoryUsage } from 'process';
 
 @Injectable()
 export class ChannelService {
-  constructor(private prisma: PrismaService) {}
-  
+  private channelMember: Record<number, string[]>;
+
+  constructor(private prisma: PrismaService) {
+    this.channelMember = {};
+  }
+
   async create(createChannelDto: CreateChannelDto) {
     var createData;
     try {
-      const {chName, intraId, chPwd} = createChannelDto;
+      const { chName, operatorId } = createChannelDto;
       createData = await this.prisma.channel.create({
-        data: { chName, chUserCnt: 0, intraId, chPwd },
+        data: { chName, chUserCnt: 1, 
+          operator: { connect: { intraId: operatorId }, } },
       });
     } catch (error) {
       if (error.code === 'P2002' && error.meta?.target?.includes('chName')) {
@@ -25,13 +36,13 @@ export class ChannelService {
     return createData;
   }
 
-  async update(id: number, updateChannelDto: UpdateChannelDto) {
+  async update(idx: number, updateChannelDto: UpdateChannelDto) {
     var updateData;
     try {
-      const {chName, intraId, chPwd} = updateChannelDto;
-      updateData = await this.prisma.channel.update({ 
-        where: { chIdx: id }, 
-        data: {chName, intraId, chPwd} 
+      const { chName, operatorId, chPwd } = updateChannelDto;
+      updateData = await this.prisma.channel.update({
+        where: { chIdx: idx },
+        data: { chName, operatorId, chPwd },
       });
     } catch (error) {
       if (error.code === 'P2002' && error.meta?.target?.includes('chName')) {
@@ -45,10 +56,10 @@ export class ChannelService {
     return updateData;
   }
 
-  async delete(id: number) {
+  async delete(idx: number) {
     var deleteData;
     try {
-      deleteData = await this.prisma.channel.delete({ where: { chIdx: id} });
+      deleteData = await this.prisma.channel.delete({ where: { chIdx: idx } });
     } catch (error) {
       if (error.code === 'P2016' || error.code === 'P2025') {
         throw new NotFoundException('channel not found');
@@ -62,28 +73,11 @@ export class ChannelService {
     return this.prisma.channel.findMany();
   }
 
-  async findOneById(id: number) {
+  async findOneById(idx: number) {
     var findData;
     try {
-      findData = await this.prisma.channel.findUnique({ 
-        where: { chIdx: id } });
-    } catch (error) {
-      if (error.code === 'P2016' || error.code === 'P2025') {
-        throw new NotFoundException('channel not found');
-      }
-      throw new InternalServerErrorException('Internal Server Error');
-    }
-    if (findData == null)
-        throw new NotFoundException('channel not found');
-    return findData;
-  }
-
-  async getChannelName(id: number) {
-    var findData;
-    try {
-      findData = await this.prisma.channel.findUnique({ 
-        where: { chIdx: id },
-        select: { chName: true }
+      findData = await this.prisma.channel.findUnique({
+        where: { chIdx: idx },
       });
     } catch (error) {
       if (error.code === 'P2016' || error.code === 'P2025') {
@@ -91,17 +85,16 @@ export class ChannelService {
       }
       throw new InternalServerErrorException('Internal Server Error');
     }
-    if (findData == null)
-        throw new NotFoundException('channel not found');
+    if (findData == null) throw new NotFoundException('channel not found');
     return findData;
   }
 
-  async getChannelUserCnt(id: number) {
+  async getChannelName(idx: number) {
     var findData;
     try {
-      findData = await this.prisma.channel.findUnique({ 
-        where: { chIdx: id },
-        select: { chUserCnt: true }
+      findData = await this.prisma.channel.findUnique({
+        where: { chIdx: idx },
+        select: { chName: true },
       });
     } catch (error) {
       if (error.code === 'P2016' || error.code === 'P2025') {
@@ -109,22 +102,87 @@ export class ChannelService {
       }
       throw new InternalServerErrorException('Internal Server Error');
     }
-    if (findData == null)
-        throw new NotFoundException('channel not found');
+    if (findData == null) throw new NotFoundException('channel not found');
     return findData;
   }
 
-  async checkPwd(pwd: string) {
-
+  async getChannelUserCnt(idx: number) {
+    var findData;
+    try {
+      findData = await this.prisma.channel.findUnique({
+        where: { chIdx: idx },
+        select: { chUserCnt: true },
+      });
+    } catch (error) {
+      if (error.code === 'P2016' || error.code === 'P2025') {
+        throw new NotFoundException('channel not found');
+      }
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    if (findData == null) throw new NotFoundException('channel not found');
+    return findData;
   }
 
-  async enter(id: number, intraId: string) {
+  // async checkPwd(pwd: string) {}
+
+  async enter(idx: number, memberIdDto: MemberIdDto) {
     // pwd 검사
-    return this.prisma.channel.update({ where: { chIdx: id }, data: { intraId: intraId }});
+    var updatedChannel;
+    var { memberId } = memberIdDto;
+    try {
+      const channel = await this.findOneById(idx);
+      if (!channel)
+        throw new NotFoundException('channel not found');
+    
+      updatedChannel = await this.prisma.channel.update({
+        where: { chIdx: idx},
+        data: {
+          chUserCnt: { increment: 1},
+        }
+      });
+      if (!(idx in this.channelMember)) {
+        this.channelMember[idx] = [];
+      }
+      this.channelMember[idx].push(memberId);
+    } catch (error) {
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    return updatedChannel;
   }
 
-  // async leave(id: number, intraId: string) {
-  //   return this.prisma.channel.delete({ where: { chIdx: id }, data: { intraId: null }});
-  // }
-  
+  async leave(idx: number, memberIdDto: MemberIdDto) {
+    var updatedChannel;
+    var { memberId } = memberIdDto;
+    try {
+      const channel = await this.findOneById(idx);
+      if (!channel)
+        throw new NotFoundException('channel not found');
+    
+      updatedChannel = await this.prisma.channel.update({
+        where: { chIdx: idx},
+        data: {
+          chUserCnt: { decrement: 1},
+        }
+      });
+      this.channelMember[idx] = this.channelMember[idx].filter(item => item !== memberId);
+
+    } catch (error) {
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    return updatedChannel;
+  }
+
+  async getChannelUsers(idx: number) {
+    var findData;
+    try {
+      findData = this.findOneById(idx);
+      if (findData == null) throw new NotFoundException('channel not found');
+    } catch (error) {
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+    console.log("get user");
+    console.log(this.channelMember[idx]);
+    return this.channelMember[idx];
+  }
+
 }
