4458c301cab2a05c28c3de81b9e35db79471de4e
jjunhyuki <yjs99004@naver.com>
2023-06-03 15:41:49 +0900

    channel_list_ing

--- frontend/src/OAuth.tsx
+++ frontend/src/OAuth.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import qs from "qs";
 import axios from "axios";
+import * as fucthion from "./Login";
 
 const OAuth: React.FC = () => {
     const location = useLocation();
