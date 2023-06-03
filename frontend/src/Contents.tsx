fdac2797efd667a61894331b688e2d02ecebc780
YH-Yoo <yehuen981@naver.com>
2023-06-03 13:35:03 +0900

    [FE][Feat] : 상단메뉴,사이드바

--- frontend/src/Contents.tsx
+++ frontend/src/Contents.tsx
@@ -1,23 +1,75 @@
+// import React, { useState } from "react";
+// import Chat from "./Chat";
+// import Sidebar from "./sidebar/Sidebar";
+// import Menu from "./menu/Menu";
+
+// type ComponentType = 'Chat' | 'Sidebar';
+
+// function Contents() {
+//   const [currentComponent, setCurrentComponent] = useState<ComponentType>('Chat');
+//   const [showBackButton, setShowBackButton] = useState(false);
+//   const [showSettingButton, setShowSettingButton] = useState(false);
+
+//   const handleComponentChange = () => {
+//     setCurrentComponent(currentComponent === 'Chat' ? 'Sidebar' : 'Chat');
+//     setShowBackButton(!showBackButton);
+//   };
+
+//   const handleBackButton = () => {
+//     setCurrentComponent('Chat');
+//     setShowBackButton(false);
+//   };
+
+//   return (
+//     <div className="contents">
+//       <div className="contents-header">
+//         <Menu showSettingButton={showSettingButton} showBackButton={showBackButton} handleBackButton={handleBackButton} />
+//       </div>
+//       <div className="contents-main">
+//         <button onClick={handleComponentChange}>dd</button>
+//         {currentComponent === 'Chat' && <Chat />}
+//         {currentComponent === 'Sidebar' && <Sidebar />}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Contents;
+
+
 import React, { useState } from "react";
 import Chat from "./Chat";
 import Sidebar from "./sidebar/Sidebar";
+import Menu from "./menu/Menu";
 
 type ComponentType = 'Chat' | 'Sidebar';
 
 function Contents() {
-        const [currentComponent, setCurrentComponent] = useState<ComponentType>('Chat');
-        const handleComponentChange = () => {
-            setCurrentComponent(currentComponent === 'Chat' ? 'Sidebar' : 'Chat');
-          };
+  const [currentComponent, setCurrentComponent] = useState<ComponentType>('Chat');
+  const [showBackButton, setShowBackButton] = useState(false);
 
-    return <div className="contents">
+  const handleComponentChange = () => {
+    setCurrentComponent(currentComponent === 'Chat' ? 'Sidebar' : 'Chat');
+    setShowBackButton(!showBackButton);
+  };
+
+  const handleBackButton = () => {
+    setCurrentComponent('Chat');
+    setShowBackButton(false);
+  };
+
+  return (
+    <div className="contents">
+      <div className="contents-header">
+        <Menu showBackButton={showBackButton} handleBackButton={handleBackButton} />
+      </div>
+      <div className="contents-main">
         <button onClick={handleComponentChange}>dd</button>
-        <div className="contents-header"></div>
-        <div className="contents-main">
         {currentComponent === 'Chat' && <Chat />}
         {currentComponent === 'Sidebar' && <Sidebar />}
-        </div>
+      </div>
     </div>
+  );
 }
 
-export default Contents
+export default Contents;
