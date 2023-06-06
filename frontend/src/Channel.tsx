3d42e66ef9396fc3ee49e2daafb4127ced7b1903
jjunhyuki <yjs99004@naver.com>
2023-06-06 21:38:55 +0900

    [FE][Design]

--- frontend/src/Channel.tsx
+++ frontend/src/Channel.tsx
@@ -10,22 +10,6 @@ function Channel(){
 
     </div>
 }
-// function Chat(){
-//     return <div className="chat">
-//         <h1>Chatting</h1>
-//         <ChatBox></ChatBox>
-//         <ChatBox></ChatBox>
-//         <ChatBox></ChatBox>
-//         <ChatInput />
-//     </div>
-// }
-
-// function ChatInput(){
-//     return <div className="chat-inputbox">
-//         <input type="text" name="chat-input" className="chat-input"></input>
-//         <Button variant="primary" className="chat-input-button">입력</Button>
-//     </div>
-// }
 
 function PwInput() {
     return <div className="pw-inputbox">
@@ -65,15 +49,5 @@ function ChannelRow() {
     </div>
 </div>
 }
-// function ChatBox(){
-//     return <div className="chat-box">
-//         <div className="chat-userimg">img</div>
-//         <div className="chat-userinfo">
-//             <span className="chat-username">사용자 이름</span>
-//             <span className="chat-userchat">사용자 채팅</span>
-//         </div>
-//     </div>
-// }
 
-// export default Cha;
 export default Channel;
