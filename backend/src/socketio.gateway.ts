6b1359337085d6940a0c7e6ea6ef89a7dd3c3f6c
Haeun Jeong <gkdms4248@gmail.com>
2023-06-16 17:15:52 +0900

    [BE][Feat] handle enter channel 추가

--- backend/src/socketio.gateway.ts
+++ backend/src/socketio.gateway.ts
@@ -9,12 +9,14 @@ import {Socket} from 'socket.io';
 export class SocketIOGateway implements OnGatewayInit, OnGatewayConnection,OnGatewayDisconnect{
   @WebSocketServer() server : Socket;
   
+
   afterInit(){
     console.log("웹소켓 서버 초기화 완료");
   }
   
   handleConnection(@ConnectedSocket() socket:Socket){
       console.log(`${socket.id} 소켓 연결`);
+      this.server["nickname"] = "anon";
   }
   
   handleDisconnect(@ConnectedSocket() socket:Socket){
@@ -26,4 +28,13 @@ export class SocketIOGateway implements OnGatewayInit, OnGatewayConnection,OnGat
     console.log('Received message:', payload);
     return 'Message received!';
   }
+
+  @SubscribeMessage('enter-channel')
+  handleChannelEnter(roomName:string, nickname:string) {
+    this.server.join(roomName);
+    this.server["nickname"] = nickname;
+    this.server.to[roomName].emit("welcome", nickname);
+    console.log(nickname);
+  }
+
 }
