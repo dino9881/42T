70a1ff16a99e1097a957fceaac6453b2ed15fca6
dino9881 <whdghthdms44@gmail.com>
2023-06-16 16:03:15 +0900

    [FE][FIX] : frontend menu 수정

--- frontend/src/Contents.tsx
+++ frontend/src/Contents.tsx
@@ -4,18 +4,18 @@ import Sidebar from "./sidebar/Sidebar";
 import Menu from "./menu/Menu";
 
 interface ContentsProps {
-  // headerComponent: React.ReactNode;
+  headerComponent: React.ReactNode;
   mainComponent: React.ReactNode;
 }
 
-function Contents ({mainComponent} : ContentsProps) {
+function Contents ({mainComponent, headerComponent} : ContentsProps) {
           return <div className="contents">
-        <div className="contents-header"></div>
+        <div className="contents-header"> {headerComponent}</div>
         <div className="contents-main">
           {mainComponent}
         </div>
     </div>
-  );
+  ;
 }
 
 export default Contents;
