fb6bba1c3ff20c6ee8270d00466b564c34021a67
lamPolar <lovast7@gmail.com>
2023-06-16 15:42:47 +0900

    [BE][Merge] : backend to main

--- backend/src/channel/channel.controller.ts
+++ backend/src/channel/channel.controller.ts
@@ -10,20 +10,14 @@ import {
 import { ChannelService } from './channel.service';
 import { CreateChannelDto } from './dto/create-channel.dto';
 import { UpdateChannelDto } from './dto/update-channel.dto';
-<<<<<<< HEAD
-=======
 import { MemberIdDto } from './dto/member-id.dto';
->>>>>>> cc30c00137afaaceb5c1eafb756723cd79e60b4f
 import {
   ApiTags,
   ApiOperation,
   ApiCreatedResponse,
   ApiResponse,
-<<<<<<< HEAD
-=======
   ApiBody,
   ApiParam,
->>>>>>> cc30c00137afaaceb5c1eafb756723cd79e60b4f
 } from '@nestjs/swagger';
 
 @Controller('channel')
@@ -49,20 +43,12 @@ export class ChannelController {
   @ApiResponse({ status: 404, description: '없는 채널 번호' })
   @ApiResponse({ status: 409, description: '중복 이름' })
   @ApiResponse({ status: 500, description: '서버 에러' })
-<<<<<<< HEAD
-  update(@Param('id') id: string, @Body() updateChannelDto: UpdateChannelDto) {
-    return this.channelService.update(+id, updateChannelDto);
-  }
-
-  @Delete(':id')
-=======
   @ApiBody({ type: CreateChannelDto })
   update(@Param('idx') idx: string, @Body() updateChannelDto: UpdateChannelDto) {
     return this.channelService.update(+idx, updateChannelDto);
   }
 
   @Delete(':idx')
->>>>>>> cc30c00137afaaceb5c1eafb756723cd79e60b4f
   @ApiResponse({ status: 200, description: '성공' })
   @ApiResponse({ status: 404, description: '없는 채널 번호' })
   @ApiResponse({ status: 500, description: '서버 에러' })
@@ -80,15 +66,18 @@ export class ChannelController {
     summary: '모든 채널 조회',
     description: 'Find All Channel API',
   })
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 404, description: '없는 채널 번호' })
+  @ApiResponse({ status: 500, description: '서버 에러' })
+  @ApiOperation({
+    summary: '모든 채널 조회',
+    description: 'Find All Channel API',
+  })
   findAll() {
     return this.channelService.findAll();
   }
 
-<<<<<<< HEAD
-  @Get(':id')
-=======
   @Get(':idx')
->>>>>>> cc30c00137afaaceb5c1eafb756723cd79e60b4f
   @ApiResponse({ status: 200, description: '성공' })
   @ApiResponse({ status: 404, description: '없는 채널 번호' })
   @ApiResponse({ status: 500, description: '서버 에러' })
@@ -96,20 +85,12 @@ export class ChannelController {
     summary: 'idx 로 한 채널 가져오기',
     description: 'Find channel By Idx API',
   })
-<<<<<<< HEAD
-  findOneById(@Param('id') id: string) {
-    return this.channelService.findOneById(+id);
-  }
-
-  @Get('/name/:id')
-=======
   @ApiParam({ name: 'idx', example: '3', description: 'Channnel Idx'})
   findOneById(@Param('idx') idx: string) {
     return this.channelService.findOneById(+idx);
   }
 
   @Get('/name/:idx')
->>>>>>> cc30c00137afaaceb5c1eafb756723cd79e60b4f
   @ApiResponse({ status: 200, description: '성공' })
   @ApiResponse({ status: 404, description: '없는 채널 번호' })
   @ApiResponse({ status: 500, description: '서버 에러' })
@@ -117,20 +98,12 @@ export class ChannelController {
     summary: 'idx 로 채널 이름 가져오기',
     description: 'Get channel name By Idx API',
   })
-<<<<<<< HEAD
-  getChannelName(@Param('id') id: string) {
-    return this.channelService.getChannelName(+id);
-  }
-
-  @Get('/user_cnt/:id')
-=======
   @ApiParam({ name: 'idx', example: '3', description: 'Channnel Idx'})
   getChannelName(@Param('idx') idx: string) {
     return this.channelService.getChannelName(+idx);
   }
 
   @Get('/user_cnt/:idx')
->>>>>>> cc30c00137afaaceb5c1eafb756723cd79e60b4f
   @ApiResponse({ status: 200, description: '성공' })
   @ApiResponse({ status: 404, description: '없는 채널 번호' })
   @ApiResponse({ status: 500, description: '서버 에러' })
@@ -138,24 +111,12 @@ export class ChannelController {
     summary: 'idx 로 채널 인원 가져오기',
     description: 'Get channel user count By Idx API',
   })
-<<<<<<< HEAD
-  getChannelUserCnt(@Param('id') id: string) {
-    return this.channelService.getChannelUserCnt(+id);
-  }
-
-  // @Post('/enter/:id')
-  // @ApiOperation({ summary: 'idx 채널에 들어가기', description: 'Enter channel By Idx API' })
-  // enterChannel(@Body() intraId: string, @Param('id') id: string) {
-  //   this.channelService.enter(+id, intraId);
-  // }
-=======
   @ApiParam({ name: 'idx', example: '3', description: 'Channnel Idx'})
   getChannelUserCnt(@Param('idx') idx: string) {
     return this.channelService.getChannelUserCnt(+idx);
   }
 
   // getChannelUser
->>>>>>> cc30c00137afaaceb5c1eafb756723cd79e60b4f
 
   @Post('/enter/:idx')
   @ApiOperation({ summary: 'idx 채널에 들어가기', description: 'Enter channel By Idx' })
