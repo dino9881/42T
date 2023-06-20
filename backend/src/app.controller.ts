d6402e2390bce6f72cc1f307259e5595139d9bea
lamPolar <lovast7@gmail.com>
2023-06-20 15:32:15 +0900

    [BE][Refactor] : auth module refactor

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,29 +1,7 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Query,
-  NotFoundException,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Post('auth')
-  @ApiResponse({
-    status: 500,
-    description: 'server error',
-  })
-  @ApiResponse({
-    status: 200,
-    description: 'success',
-  })
-  recieveCode(@Body('code') code: string) {
-    console.log(code);
-    this.appService.getIntraAccessToken(code);
-  }
 }
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,77 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { MemberService } from '../member/member.service';
+import { JwtService } from '@nestjs/jwt';
+import { Member } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
+import axios, { AxiosResponse } from 'axios';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private memberService: MemberService,
+    private jwtService: JwtService,
+    public readonly config: ConfigService,
+  ) {}
+
+  public async getIntraAccessToken(code) {
+    const getTokenUrl = 'https://api.intra.42.fr/oauth/token';
+    const body = {
+      grant_type: 'authorization_code',
+      code: code,
+      client_id: this.config.get('CLIENT_ID'),
+      client_secret: this.config.get('CLIENT_SECRET'),
+      redirect_uri: 'http://localhost:3000/login',
+    };
+    const response: AxiosResponse = await axios.post(getTokenUrl, body);
+    if (response.data.error) {
+      throw new UnauthorizedException('intra 인증 실패');
+    }
+    const { access_token } = response.data;
+    const { data } = await axios.get('https://api.intra.42.fr/v2/me', {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    return data.login;
+  }
+
+  async generateAccessToken(member: Member): Promise<string> {
+    const payload = {
+      intraId: member.intraId,
+    };
+    return await this.jwtService.signAsync(payload);
+  }
+
+  async generateRefreshToken(member: Member): Promise<string> {
+    const payload = {
+      intraId: member.intraId,
+    };
+    return await this.jwtService.signAsync(
+      { intraId: payload.intraId },
+      {
+        secret: this.config.get<string>('JWT_REFRESH_SECRET'),
+        expiresIn: this.config.get<string>('JWT_REFRESH_EXPIRATION_TIME'),
+      },
+    );
+  }
+
+  async refresh(refreshToken: string) {
+    // Verify refresh token
+    // JWT Refresh Token 검증 로직
+    const decodedRefreshToken = this.jwtService.verify(refreshToken, {
+      secret: this.config.get<string>('JWT_REFRESH_SECRET'),
+    });
+    // user가 존재하는지 체크 (refresh token이 유효한지)
+    const intraId: string = decodedRefreshToken.intraId;
+    const member = await this.memberService.getMemberIfRefreshTokenMatches(
+      refreshToken,
+      intraId,
+    );
+    if (member == null) {
+      throw new UnauthorizedException('Invalid user!');
+    }
+    // new access token 생성
+    const accessToken = await this.generateAccessToken(member);
+    return accessToken;
+  }
+}
