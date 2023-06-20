4835b76654f012f41c53afe564210e68a6823b44
lamPolar <lovast7@gmail.com>
2023-06-20 15:32:15 +0900

    [BE][Refactor] : auth module refactor

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,107 @@
+import {
+  Body,
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+  Get,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Response, Request } from 'express';
+import { AuthService } from './auth.service';
+// import { AuthGuard } from './auth.guard';
+import { MemberService } from 'src/member/member.service';
+// import { RefreshTokenDto } from './refresh-token.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('auth')
+@ApiResponse({
+  status: 500,
+  description: '서버 에러',
+})
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private memberService: MemberService,
+  ) {}
+
+  @ApiOperation({ summary: '42intra인증 코드 전송' })
+  @Post('code')
+  @ApiResponse({
+    status: 201,
+    description: '인증 성공',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'intra 인증실패',
+  })
+  @ApiBody({
+    schema: {
+      properties: {
+        code: { example: 'intra-code', type: 'string' },
+      },
+    },
+    required: true,
+    description: 'intra code',
+  })
+  recieveCode(@Body('code') code: string) {
+    console.log(code);
+    return this.authService.getIntraAccessToken(code);
+  }
+
+  @ApiOperation({ summary: '로그인시 access & refresh token 발급' })
+  // @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async signIn(
+    @Body() loginDto: Record<string, any>,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const member = await this.memberService.getOne(loginDto.intraId);
+    const access_token = await this.authService.generateAccessToken(member);
+    const refresh_token = await this.authService.generateRefreshToken(member);
+    await this.memberService.setCurrentRefreshToken(
+      refresh_token,
+      member.intraId,
+    );
+    res.setHeader('Authorization', 'Bearer ' + [access_token]);
+    res.cookie('refresh_token', refresh_token, {
+      httpOnly: true,
+      path: '/',
+      domain: 'localhost',
+      maxAge: 60 * 60 * 24 * 7, // week
+    });
+    res.status(200).json({
+      message: 'login success',
+      access_token: access_token,
+    });
+  }
+
+  // @UseGuards(AuthGuard)
+  // @Get('profile')
+  // getProfile(@Body() body) {
+  //   return body.intraId;
+  // }
+
+  @ApiOperation({ summary: 'refresh token으로 access token재발급' })
+  @Post('refresh')
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    try {
+      const refreshToken = req.cookies['refresh_token'];
+      const newAccessToken = await this.authService.refresh(refreshToken);
+      res.setHeader('Authorization', 'Bearer' + newAccessToken);
+      res.status(200).json({
+        message: 'refresh success',
+        access_token: newAccessToken,
+      });
+    } catch (err) {
+      throw new UnauthorizedException('Invalid refresh-token');
+    }
+  }
+}
---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,34 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MemberModule } from '../member/member.module';
+import { JwtModule } from '@nestjs/jwt';
+// import { APP_GUARD } from '@nestjs/core';
+// import { AuthGuard } from './auth.guard';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    MemberModule,
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_ACCESS_SECRET'),
+        global: true,
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_ACCESS_EXPIRATION_TIME'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    forwardRef(() => MemberModule),
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    // {
+    //   provide: APP_GUARD,
+    //   // useClass: AuthGuard,
+    // },
+  ],
+})
+export class AuthModule {}
