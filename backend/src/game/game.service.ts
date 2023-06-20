004ca7e5a1a98d8a9fd326a0828c2d4dc8d9fb68
lamPolar <lovast7@gmail.com>
2023-06-20 15:34:46 +0900

    [BE][Feat] : game module 생성 & 전적 추가 & 가져오기 기능 생성

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
-import { UpdateGameDto } from './dto/update-game.dto';
+import { PrismaService } from 'src/prisma.service';
+import { MemberService } from 'src/member/member.service';
 
 @Injectable()
 export class GameService {
-  create(createGameDto: CreateGameDto) {
-    return 'This action adds a new game';
+  constructor(
+    private prisma: PrismaService,
+    private memberService: MemberService,
+  ) {}
+
+  async addHistory(game: CreateGameDto) {
+    // id1 , id2 가 있는 멤버인지 확인
+    await this.memberService.getOne(game.winnerId);
+    await this.memberService.getOne(game.loserId);
+    // winnerScore가 loserScore보다 큰지 확인
+    if (game.winnerScore < game.loserScore) {
+      throw new HttpException(
+        'winnerScore must be bigger than loserScore',
+        416,
+      );
+    }
+    // gamehistory db 접근
+    await this.prisma.gameHistory.create({
+      data: game,
+    });
+    //member rank 업데이트하기
+    //winner : +5
+    await this.memberService.updateRank(game.winnerId, 5);
+    //loser : +3
+    await this.memberService.updateRank(game.loserId, 3);
+    return;
   }
 
-  findAll() {
-    return `This action returns all game`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} game`;
-  }
-
-  update(id: number, updateGameDto: UpdateGameDto) {
-    return `This action updates a #${id} game`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} game`;
+  async findGamesByIntraId(intraId: string) {
+    //intraId가 있는 멤버인지 확인
+    await this.memberService.getOne(intraId);
+    //gamehistory db 접근 : 5개 가져오기
+    const games = await this.prisma.gameHistory.findMany({
+      where: {
+        OR: [{ winnerId: intraId }, { loserId: intraId }],
+      },
+      orderBy: {
+        gameIdx: 'desc',
+      },
+      take: 5,
+    });
+    //총 개수, 데이터를 리턴
+    return {
+      length: games.length,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      data: games.map(({ gameIdx, ...gameDetail }) => gameDetail),
+    };
   }
 }
