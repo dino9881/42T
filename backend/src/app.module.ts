a8754d0f5e0c414962712d0c3741a1966fb92aaf
lamPolar <lovast7@gmail.com>
2023-06-20 19:48:34 +0900

    [BE][Rename] : prisma module 생성 및 관련 import 수정2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,24 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
 import { MemberModule } from './member/member.module';
 import { GameModule } from './game/game.module';
 import { ChannelModule } from './channel/channel.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { SocketIOGateway } from './socketio.gateway';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
-    MemberModule,
+    AuthModule,
     ChannelModule,
     GameModule,
+    MemberModule,
+    PrismaModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '../.env',
     }),
-    AuthModule,
   ],
-  providers: [PrismaService, SocketIOGateway],
+  providers: [SocketIOGateway],
 })
 export class AppModule {}
