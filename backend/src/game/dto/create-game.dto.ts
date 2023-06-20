88454c85f817469ea926d87dd34c3a5180adfa89
lamPolar <lovast7@gmail.com>
2023-06-20 15:34:46 +0900

    [BE][Feat] : game module 생성 & 전적 추가 & 가져오기 기능 생성

--- backend/src/game/dto/create-game.dto.ts
+++ backend/src/game/dto/create-game.dto.ts
@@ -1,1 +1,36 @@
-export class CreateGameDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateGameDto {
+  @ApiProperty({
+    example: 'heeskim',
+    description: '게임을 이긴 유저의 인트라아이디',
+    required: true,
+  })
+  @IsString()
+  winnerId: string;
+
+  @ApiProperty({
+    example: 'yyoo',
+    description: '게임을 진 유저의 인트라아이디',
+    required: true,
+  })
+  @IsString()
+  loserId: string;
+
+  @ApiProperty({
+    example: 1,
+    description: '게임을 이긴 유저의 점수',
+    required: true,
+  })
+  @IsNumber()
+  winnerScore: number;
+
+  @ApiProperty({
+    example: 2,
+    description: '게임을 진 유저의 점수',
+    required: true,
+  })
+  @IsNumber()
+  loserScore: number;
+}
--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { PrismaService } from 'src/prisma.service';
+import { MemberService } from 'src/member/member.service';
 
 @Module({
   controllers: [GameController],
-  providers: [GameService]
+  providers: [GameService, PrismaService, MemberService],
 })
 export class GameModule {}
