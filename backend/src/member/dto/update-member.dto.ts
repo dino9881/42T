c0f163ae755869fd3b71a039e829fa4b45808c0c
lamPolar <lovast7@gmail.com>
2023-06-24 13:08:18 +0900

    [BE][Refactor] : friend table 삭제 및 관련 dto 수정

--- backend/src/member/dto/update-member.dto.ts
+++ backend/src/member/dto/update-member.dto.ts
@@ -1,4 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateMemberDto } from './create-member.dto';
+import { MemberInfoDto } from './member-info.dto';
 
-export class UpdateMemberDto extends PartialType(CreateMemberDto) {}
+export class UpdateMemberDto extends PartialType(MemberInfoDto) {}
