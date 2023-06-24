b052b2ea09e073c1dab9017488efb14426192f92
lamPolar <lovast7@gmail.com>
2023-06-24 13:14:00 +0900

    [FE][Merge] : channel 디렉토리 merge

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import Sidebar from "./sidebar/Sidebar";
 import Contents from "./Contents";
-<<<<<<< HEAD
-import Channel from "./Channel";
-=======
 import Channel from "./channel/Channel";
->>>>>>> channel
 import Menu from "./menu/Menu";
-function Main(){
-   
-    return <div>
-         <Contents headerComponent={<Menu showBackButton={false}/>} mainComponent={<Channel/>}/>
-         <Sidebar />
-    </div>
-    ;
+function Main() {
+    return (
+        <div>
+            <Contents
+                headerComponent={<Menu showBackButton={false} />}
+                mainComponent={<Channel />}
+            />
+            <Sidebar />
+        </div>
+    );
 }
 
 export default Main;
