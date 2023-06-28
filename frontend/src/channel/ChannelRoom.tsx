0662dc34dc39d959cdea0fe45cc8660ba0bdfb2d
jjunhyuki <yjs99004@naver.com>
2023-06-28 20:59:41 +0900

    [FE][Feat] :비밀번호에 따라서 입력창이 나타나거나 아니거나

--- frontend/src/channel/ChannelRoom.tsx
+++ frontend/src/channel/ChannelRoom.tsx
@@ -21,17 +21,20 @@ function ChannelRoom({ channelData }: ChannelRoomProps) {
     return (
         <div className="chan-room">
             <div className="chan-lockcheck">
-                <img src="/channel/lock.png" alt="lock" />
+                {/* {chPwd && (<img src="/channel/lock.png" alt="lock" />)}
+                {!chPwd && (<img src="/channel/lock_not.png" alt="open_lock"/>)} */}
+                {chPwd ? <img src="/channel/lock.png" alt="lock" /> : <img src="/channel/lock_not.png" alt="open_lock"/>}
             </div>
             <div className="chan-hostinfo">
                 <div className="chan-hostimg">
                     <img src="/avatar/son.jpeg" alt="son" />
                 </div>
-                <span className="chan-hostname">닉네임!!</span>
+                <span className="chan-hostname">{operatorId}</span>
             </div>
             <div className="chan-info">
                 <span className="chan-name">{chName}</span>
-                <PwInput chIdx={chIdx} />
+                
+                    <PwInput chIdx={chIdx} chPwd={chPwd} />
                 <div className="chan-count">{chUserCnt}/5</div>
             </div>
         </div>
--- frontend/src/channel/PwInput.tsx
+++ frontend/src/channel/PwInput.tsx
@@ -1,31 +1,67 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 interface PwInputProps {
     chIdx: number;
+    chPwd: string;
 }
 
-function PwInput({ chIdx }: PwInputProps) {
+function PwInput({ chIdx, chPwd } : PwInputProps) {
     const navigate = useNavigate();
+    const [inputValue, setInputValue] = useState("");
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        // 숫자 이외의 문자 제거
+        const filteredValue = value.replace(/\D/g, "");
+        setInputValue(filteredValue);
+    };
+
+    const pwCorrect = () : Promise<boolean> => {
+        return axios
+            .post("http://localhost:5001/channel/check" ,{
+                inputValue: inputValue,
+                chIdx: chIdx,
+            })
+            .then((response) => {
+                const data = response.data;
+                return data;
+            })
+            .catch((error) => {
+                // 요청이 실패하면 에러 처리
+                console.error("API 요청 실패:", error);
+            });
+    }
 
     const handleButtonClick = () => {
-        navigate("/chat", { state: { chIdx } });
+        // 입력값이 4자리인지 확인
+        if(!chPwd)
+        {   navigate("/chat", { state: { chIdx } });
+            return 
+        }
+        if (inputValue.length === 4) {
+            pwCorrect().then((data: boolean) => {
+                if (data) {
+                    navigate("/chat", { state: { chIdx } });
+                } else {
+                    alert("비밀번호를 확인해주세요. ^^");
+                }
+            });
+        } else {
+            alert("비밀번호를 확인해주세요. ^^");
+        }
     };
 
     return (
-        <div className="pw-inputbox">
-            <input type="text" name="pw-input" className="pw-input" />
-            <Button
-                onClick={handleButtonClick}
-                variant="light"
-                className="pw-input-button"
-                size="sm"
-            >
-                Enter
+        <div className="chan-pw_inputbox">
+            <input type="password" name="pw-input" className={chPwd ? "chan-pw_input" : "chan-pw_input hidden"} value={inputValue} onChange={handleInputChange} maxLength={4} />
+            <Button onClick={handleButtonClick} className="enter_button" variant="light" size="sm">
+              Enter
             </Button>
         </div>
-    );
+      );
 }
 
 export default PwInput;
