5f896541f1909ec0eb8d1f54691eba001ea112cb
lamPolar <lovast7@gmail.com>
2023-07-07 15:25:02 +0900

    [BE][Docs] : comment 수정 및 import path 수정

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -6,7 +6,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MemberModule } from '../member/member.module';
 import { JwtStrategy } from './jwt.strategy';
-import { PrismaService } from '../prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [
