b40f64a61f5fe97d1685d74ab5419a4654d77f20
lamPolar <lovast7@gmail.com>
2023-07-07 16:04:46 +0900

    [BE][Fix] : prisma 파일 이동으로 import path변경

--- backend/src/channel/channel.module.ts
+++ backend/src/channel/channel.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { ChannelController } from './channel.controller';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [ChannelController],
