58e655d53c358a28b658fcba06b3b5fd8bc58ccd
lamPolar <lovast7@gmail.com>
2023-07-10 15:10:37 +0900

    [BE][Feat] : status update 부분 코드 수정 (socket)

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -18,47 +18,33 @@ export class GameService {
   async addHistory(game: CreateGameDto) {
     if (game.winnerId == game.loserId)
       throw new ConflictException('P1 and P2 are same Member');
-    // id1 , id2 가 있는 멤버인지 확인
     await this.memberService.getOne(game.winnerId);
     await this.memberService.getOne(game.loserId);
-    // winnerScore가 loserScore보다 큰지 확인
     if (game.winnerScore < game.loserScore) {
       throw new HttpException(
         'winnerScore must be bigger than loserScore',
         416,
       );
     }
-    // gamehistory db 접근
-    await this.prisma.gameHistory.create({
-      data: game,
-    });
-    //member rank 및 전적 업데이트하기
-    //winner : +5
+    await this.prisma.gameHistory.create({ data: game });
     await this.memberService.updateRank(game.winnerId, 5);
     await this.memberService.updateWinCnt(game.winnerId);
     await this.memberService.updateStatus(game.winnerId, 1);
-    //loser : +3
     await this.memberService.updateRank(game.loserId, 3);
     await this.memberService.updateLoseCnt(game.loserId);
     await this.memberService.updateStatus(game.loserId, 1);
-
     return;
   }
 
   async findGamesByIntraId(intraId: string) {
-    //intraId가 있는 멤버인지 확인
     await this.memberService.getOne(intraId);
-    //gamehistory db 접근 : 5개 가져오기
     const games = await this.prisma.gameHistory.findMany({
       where: {
         OR: [{ winnerId: intraId }, { loserId: intraId }],
       },
-      orderBy: {
-        gameIdx: 'desc',
-      },
+      orderBy: { gameIdx: 'desc' },
       take: 5,
     });
-    //총 개수, 데이터를 리턴
     return games;
   }
 
