a7b3ac765ee7b07d943ff8130adcf9043f7f211b
lamPolar <lovast7@gmail.com>
2023-07-11 12:52:47 +0900

    [BE][Fix] : backend 의존성 문제 및 return type관련 해결

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -6,5 +6,6 @@ import { MemberService } from 'src/member/member.service';
 @Module({
   controllers: [GameController],
   providers: [GameService, MemberService],
+  exports: [GameService],
 })
 export class GameModule {}
