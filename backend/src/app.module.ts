7ddf95c5aef71d7883de08514fcbc82badb59c79
lamPolar <lovast7@gmail.com>
2023-07-13 20:18:45 +0900

    [BE][Refactor] : socketio module 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,14 +4,14 @@ import { GameModule } from './game/game.module';
 import { ChannelModule } from './channel/channel.module';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { SocketIOGateway } from './socketio.gateway';
 import { PrismaModule } from './prisma/prisma.module';
 import { MailModule } from './mail.module';
-import { Socket } from 'socket.io';
 import { MemberService } from './member/member.service';
+import { SocketIOModule } from './socketio.module';
 
 @Module({
   imports: [
+    SocketIOModule,
     AuthModule,
     ChannelModule,
     GameModule,
@@ -23,13 +23,7 @@ import { MemberService } from './member/member.service';
     }),
     MailModule,
   ],
-  providers: [
-    SocketIOGateway,
-    {
-      provide: Map,
-      useValue: new Map<string, Socket>(),
-    },
-  ],
+  providers: [],
 })
 export class AppModule implements OnModuleInit {
   constructor(private readonly memberService: MemberService) {}
