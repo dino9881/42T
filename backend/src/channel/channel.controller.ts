b9af713436101fc6c8179ff93c77d31442da92f8
Haeun Jeong <gkdms4248@gmail.com>
2023-07-17 15:18:40 +0900

    [BE][Feat] : private channel 추가, leave socket 으로 수정

--- backend/src/channel/channel.controller.ts
+++ backend/src/channel/channel.controller.ts
@@ -36,18 +36,16 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 export class ChannelController {
   constructor(private readonly channelService: ChannelService) {}
 
-  // @ApiTags('Channel')
-  // @Post('/create')
-  // @ApiOperation({ summary: '채널 생성', description: 'Create Channel API' })
-  // @ApiResponse({ status: 400, description: '잘못된 요청' })
-  // @ApiResponse({ status: 409, description: '중복 이름' })
-  // @ApiCreatedResponse({ type: CreateChannelDto })
-  // @ApiBody({ type: CreateChannelDto })
-  // create(
-  //   @GetMember() member: MemberInfoDto,
-  //   @Body() createChannelDto: CreateChannelDto,
-  // ) {
-  //   return this.channelService.create(member, createChannelDto);
+  
+
+  // @ApiTags('Channel User')
+  // @Post('/leave/:idx')
+  // @ApiOperation({
+  //   summary: 'idx 채널에서 나오기',
+  //   description: 'Leave channel By Idx',
+  // })
+  // leaveChannel(@GetMember() member: MemberInfoDto, @Param('idx') idx: string) {
+  //   return this.channelService.leave(+idx, member.intraId);
   // }
 
   @ApiTags('Channel')
@@ -91,17 +89,6 @@ export class ChannelController {
   }
 
   @ApiTags('Channel')
-  @Get('dm/all')
-  @ApiCreatedResponse({ type: UpdateChannelDto })
-  @ApiOperation({
-    summary: '모든 DM 채널 조회',
-    description: 'Find DM Channel API',
-  })
-  findDMAll() {
-    return this.channelService.findDMAll();
-  }
-
-  @ApiTags('Channel')
   @Get(':idx')
   @ApiOperation({
     summary: 'idx 로 한 채널 가져오기',
@@ -119,7 +106,7 @@ export class ChannelController {
     summary: '채널이름으로 한 채널 가져오기',
     description: 'Find channel By Name',
   })
-  @ApiParam({ name: 'name', example: 'channel', description: 'Channnel name' })
+  @ApiParam({ name: 'name', example: 'channel', description: 'Channnel Name' })
   @ApiCreatedResponse({ type: UpdateChannelDto })
   findOneByName(@Param('name') name: string) {
     return this.channelService.findOneByName(name);
@@ -177,13 +164,13 @@ export class ChannelController {
   @ApiTags('Channel')
   @Post('/oper/:idx')
   @ApiOperation({
-    summary: 'idx 채널의 operator 인지 확인',
-    description: 'Is operator',
+    summary: 'idx 채널의 owner 인지 확인',
+    description: 'Is owner',
   })
   @ApiBody({ type: MemberInfoDto })
   @ApiParam({ name: 'idx', example: '3', description: 'Channnel Idx' })
-  isOperator(@GetMember() member: MemberInfoDto, @Param('idx') idx: string) {
-    return this.channelService.isOperator(+idx, member.intraId);
+  isOwner(@GetMember() member: MemberInfoDto, @Param('idx') idx: string) {
+    return this.channelService.isOwner(+idx, member.intraId);
   }
 
   @ApiTags('Channel')
@@ -196,22 +183,6 @@ export class ChannelController {
   @ApiParam({ name: 'idx', example: '3', description: 'Channnel Idx' })
   isAdmin(@GetMember() member: MemberInfoDto, @Param('idx') idx: string) {
     return this.channelService.isAdmin(+idx, member.intraId);
-  }
-
-  @ApiTags('Channel')
-  @Post('/admin/set/:idx')
-  @ApiOperation({
-    summary: 'idx 채널의 administrator 저장',
-    description: 'set administrator',
-  })
-  @ApiBody({ type: MemberInfoDto })
-  @ApiParam({ name: 'idx', example: '3', description: 'Channnel Idx' })
-  setAdmin(
-    @GetMember() member: MemberInfoDto,
-    @Param('idx') idx: string,
-    @Body() channelUserDto: ChannelUserDto,
-  ) {
-    return this.channelService.setAdmin(+idx, member.intraId, channelUserDto);
   }
 
   @ApiTags('Channel')
@@ -240,33 +211,6 @@ export class ChannelController {
   }
 
   @ApiTags('Channel User')
-  @Post('/leave/:idx')
-  @ApiOperation({
-    summary: 'idx 채널에서 나오기',
-    description: 'Leave channel By Idx',
-  })
-  leaveChannel(@GetMember() member: MemberInfoDto, @Param('idx') idx: string) {
-    return this.channelService.leave(+idx, member.intraId);
-  }
-
-  // @ApiTags('Channel User')
-  // @Post('/kick/:idx')
-  // @ApiResponse({ status: 403, description: '권한 없음' })
-  // @ApiOperation({
-  //   summary: 'idx 채널의 user 쫓아내기',
-  //   description: 'Is operator',
-  // })
-  // @ApiBody({ type: ChannelUserDto })
-  // @ApiParam({ name: 'idx', example: '3', description: 'Channnel Idx' })
-  // kickChannel(
-  //   @GetMember() member: MemberInfoDto,
-  //   @Body() channelUserDto: ChannelUserDto,
-  //   @Param('idx') idx: string,
-  // ) {
-  //   return this.channelService.kick(+idx, member.intraId, channelUserDto);
-  // }
-
-  @ApiTags('Channel User')
   @Get('/users/:idx')
   @ApiOperation({
     summary: 'idx 채널 유저들 가져오기',
@@ -291,37 +235,6 @@ export class ChannelController {
   }
 
   // ban
-
-  // @ApiTags('Channel User')
-  // @Post('/ban/save/:idx')
-  // @ApiResponse({ status: 403, description: '권한 없음' })
-  // @ApiOperation({
-  //   summary: 'idx 채널에 밴 유저 등록하기',
-  //   description: 'Save ban user',
-  // })
-  // @ApiBody({ type: ChannelUserDto })
-  // @ApiParam({ name: 'idx', example: '3', description: 'Channel Idx' })
-  // saveBanUser(
-  //   @Body() channelUserDto: ChannelUserDto,
-  //   @Param('idx') idx: string,
-  // ) {
-  //   return this.channelService.saveBanUser(+idx, channelUserDto);
-  // }
-
-  @ApiTags('Channel User')
-  @Post('/ban/delete/:idx')
-  @ApiOperation({
-    summary: 'idx 채널에서 밴 유저 삭제하기',
-    description: 'Delete ban user',
-  })
-  @ApiBody({ type: ChannelUserDto })
-  @ApiParam({ name: 'idx', example: '3', description: 'Channel Idx' })
-  deleteBanUser(
-    @Body() channelUserDto: ChannelUserDto,
-    @Param('idx') idx: string,
-  ) {
-    return this.channelService.deleteBanUser(+idx, channelUserDto);
-  }
 
   @ApiTags('Channel User')
   @Get('/ban/:idx')
@@ -349,25 +262,11 @@ export class ChannelController {
     return this.channelService.getMessageList(+idx, member);
   }
 
-  // mute
-  // @ApiTags('Channel User')
-  // @Post('/mute/:idx')
-  // @ApiResponse({ status: 403, description: '권한 없음' })
-  // @ApiOperation({
-  //   summary: 'idx 채널의 유저 mute 하기',
-  //   description: 'Mute user',
-  // })
-  // @ApiBody({ type: ChannelUserDto })
-  // muteUser(@Param('idx') idx: string, @Body() channelUserDto: ChannelUserDto) {
-  //   this.channelService.muteUser(+idx, channelUserDto);
-  // }
-
   // DM
 
   @ApiTags('Channel DM')
   @Post('/enter/dm/chan')
   @ApiOperation({ summary: 'DM 채널 생성', description: 'Create Channel API' })
-  @ApiResponse({ status: 400, description: '잘못된 요청' })
   @ApiCreatedResponse({ type: CreateChannelDto })
   @ApiBody({ type: ChannelUserDto })
   enterDM(
@@ -385,5 +284,54 @@ export class ChannelController {
   })
   getMyDMChannels(@GetMember() member: MemberInfoDto) {
     return this.channelService.getMyDMChannels(member.intraId);
+  }
+
+  
+  @ApiTags('Channel DM')
+  @Get('dm/all')
+  @ApiCreatedResponse({ type: UpdateChannelDto })
+  @ApiOperation({
+    summary: '모든 DM 채널 조회',
+    description: 'Find DM Channel API',
+  })
+  findDMAll() {
+    return this.channelService.findDMAll();
+  }
+
+  // private
+
+  @ApiTags('Channel Private')
+  @Post('/create')
+  @ApiOperation({ summary: 'private 채널 생성할 때 사용', description: 'Create Channel' })
+  @ApiResponse({ status: 400, description: '잘못된 요청' })
+  @ApiResponse({ status: 409, description: '중복 이름' })
+  @ApiCreatedResponse({ type: CreateChannelDto })
+  @ApiBody({ type: CreateChannelDto })
+  create(
+    @GetMember() member: MemberInfoDto,
+    @Body() createChannelDto: CreateChannelDto,
+  ) {
+    return this.channelService.create(member, createChannelDto);
+  }
+
+  @ApiTags('Channel Private')
+  @Get('private/all')
+  @ApiCreatedResponse({ type: UpdateChannelDto })
+  @ApiOperation({
+    summary: '모든 Private 채널 조회',
+    description: 'Find Private Channel API',
+  })
+  findPrivateChannelAll() {
+    return this.channelService.findPrivateChannelAll();
+  }
+  
+  @ApiTags('Channel Private')
+  @Get('/my/private')
+  @ApiOperation({
+    summary: '나의 모든 Private 채널 가져오기',
+    description: 'Get my all Pravate channel',
+  })
+  getMyPrivateChannels(@GetMember() member: MemberInfoDto) {
+    return this.channelService.getMyPrivateChannels(member.intraId);
   }
 }
