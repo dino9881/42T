8b65853c5af94b1542853d71b3a6355e75876baa
lamPolar <lovast7@gmail.com>
2023-07-20 13:32:45 +0900

    [BE][Feat] : game custom mode easy 구현

---
+++ backend/src/game/game.Interface.ts
@@ -0,0 +1,26 @@
+import { Socket } from 'socket.io';
+
+export interface Ball {
+  x: number;
+  y: number;
+  dx: number;
+  dy: number;
+}
+
+export interface GameProps {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+  ball: Ball;
+  p1Score: number;
+  p2Score: number;
+  speed: number;
+}
+
+export interface GameRoom {
+  player1: Socket;
+  player2: Socket;
+  gameProps: GameProps;
+  mode: number;
+}
