ea3009a5b1321e2b5294b455269e22585035585b
Haeun Jeong <gkdms4248@gmail.com>
2023-08-15 14:09:07 +0900

    [CM][HotFix] : game paddle이 반대로 뜨는 오류 수정

--- frontend/src/game/Game.tsx
+++ frontend/src/game/Game.tsx
@@ -89,10 +89,14 @@ function Game() {
               const player2Avatar = response.data.avatar;
               setPlayer2Avatar(player2Avatar);
               socket.on("game-end", (payload:ScoreProps) => {
+                console.log("end : ");
+                console.log(payload);
                 navigate("/result", { state: { player1, player2, player1Avatar, player2Avatar, p1Score:payload.p1Score, p2Score:payload.p2Score} })
                 });
-            
+                
                 socket.on("game-sudden-end", (payload:ScoreProps) => {
+                console.log("sudden-end");
+                console.log(payload);
                 navigate("/result", { state: { player1, player2, player1Avatar, player2Avatar, p1Score:payload.p1Score, p2Score:payload.p2Score} })
                 });
           })
@@ -128,8 +132,10 @@ function Game() {
     })
     
     socket.on("game-score", (payload:ScoreProps) => {
-    setP1Score(payload.p1Score);
-    setP2Score(payload.p2Score);
+        console.log("game-score");
+        console.log(payload);
+        setP1Score(payload.p1Score);
+        setP2Score(payload.p2Score);
     });
 
     socket.on("game-exception" ,() => {
